a25b9da7123964c7daaca935c8d43b63e0156e86
Milan Slavicinsky <mis@isnordic.dk>
2020-06-14 22:23:28 +0200

    update tests on report service

--- src/test/groovy/net/jsreport/java/service/ReportingServiceImplSpec.groovy
+++ src/test/groovy/net/jsreport/java/service/ReportingServiceImplSpec.groovy
@@ -21,53 +21,6 @@ class ReportingServiceImplSpec extends Specification {
     private static final PDFTextStripper PDF_TEXT_STRIPPER = new PDFTextStripper()
     private static final BasicHeader CONTENT_TYPE_HEADER = new BasicHeader(HttpRemoteServiceImpl.HEADER_CONTENT_TYPE, "")
 
-    HttpRemoteService HTTP_REMOTE_SERVICE_MOCK = Mock(HttpRemoteService) {
-        post("/api/report", _) >> { def args ->
-                HttpResponse httpResponseMock = Mock(HttpResponse)
-
-                RenderTemplateRequest req = GSON.fromJson(
-                        new StringReader(args[1] as String),
-                        RenderTemplateRequest.class
-                )
-
-                if (req.template.name.startsWith("code_")) {
-                    httpResponseMock.getStatusLine() >> { return new BasicStatusLine(new ProtocolVersion("HTTP", 1, 1), Integer.parseInt(req.template.name.substring(5)), "Test") }
-
-                } else if (req.template.name.startsWith("ex_")) {
-                    String exType = req.template.name.substring(3)
-                    switch (exType) {
-                        case "UnsupportedEncodingException": throw new UnsupportedEncodingException()
-                        case "URISyntaxException": throw new URISyntaxException("test", "test")
-                        case "JsReportException": throw new JsReportException()
-                        default: throw new JsReportException()
-                    }
-                }
-
-                HttpEntity httpEntityMock = Mock(HttpEntity)
-                httpResponseMock.getStatusLine() >> { return new BasicStatusLine(new ProtocolVersion("HTTP", 1, 1), 200, "Test") }
-                httpEntityMock.getContent() >> { return ReportingServiceImplSpec.getResourceAsStream("${req.template.name}.pdf") }
-                httpResponseMock.getEntity() >> { return httpEntityMock }
-
-                return httpResponseMock
-        }
-
-        findHeader(_, HttpRemoteServiceImpl.HEADER_CONTENT_TYPE) >> { return CONTENT_TYPE_HEADER }
-
-        findAndParseHeader(_, HttpRemoteServiceImpl.HEADER_FILE_EXTENSION) >> { return "" }
-    }
-
-    private void assertReport(Report report, String requiredText) {
-        assert report != null
-        assert report.content != null
-        assert report.fileExtension == ""
-        assert report.contentType == CONTENT_TYPE_HEADER
-        assert report.getPermanentLink() == null
-
-        def load = PDDocument.load(report.content)
-        assert requiredText == PDF_TEXT_STRIPPER.getText(load)
-        load.close()
-    }
-
     @Unroll
     def testRender() {
         setup:
@@ -98,8 +51,140 @@ class ReportingServiceImplSpec extends Specification {
         "ok"                                    | "Simple test of template call!\n"     | null
         "data"                                  | "Hello jsreport!\n"                   | [ "user" : "jsreport" ]
         "ex_UnsupportedEncodingException"       | null                                  | null
+        "ex_JsReportException"                  | null                                  | null
+        "code_500"                              | null                                  | null
+        "code_404"                              | null                                  | null
+        "code_400"                              | null                                  | null
+    }
+
+    @Unroll
+    def testRenderByShortIdAndSerializedJson() {
+        when:
+
+        ReportingService reportingService = new ReportingServiceImpl(HTTP_REMOTE_SERVICE_MOCK)
+
+        then:
+
+        Report report
+        try {
+            report = reportingService.render(name, GSON.toJson(data))
+        } catch(JsReportException e) {
+            if (name.startsWith("code_")) return
+            if (name.contains(e.class.simpleName)) return
+            if (e.cause != null && name.contains(e.cause.class.simpleName)) return
+
+            assert false : "Invalid exception"
+        }
+
+        assertReport(report, text)
+
+        where:
+
+        name                                    | text                                  | data
+        "ok"                                    | "Simple test of template call!\n"     | null
+        "data"                                  | "Hello jsreport!\n"                   | [ "user" : "jsreport" ]
+        "ex_UnsupportedEncodingException"       | null                                  | null
         "ex_URISyntaxException"                 | null                                  | null
         "ex_JsReportException"                  | null                                  | null
         "code_500"                              | null                                  | null
+        "code_404"                              | null                                  | null
+        "code_400"                              | null                                  | null
     }
+
+    @Unroll
+    def testRenderByShortIdAndObject() {
+        when:
+
+        ReportingService reportingService = new ReportingServiceImpl(HTTP_REMOTE_SERVICE_MOCK)
+
+        then:
+
+        Report report
+        try {
+            report = reportingService.render(name, data)
+        } catch(JsReportException e) {
+            if (name.startsWith("code_")) return
+            if (name.contains(e.class.simpleName)) return
+            if (e.cause != null && name.contains(e.cause.class.simpleName)) return
+
+            assert false : "Invalid exception"
+        }
+
+        assertReport(report, text)
+
+        where:
+
+        name                                    | text                                  | data
+        "ok"                                    | "Simple test of template call!\n"     | null
+        "data"                                  | "Hello jsreport!\n"                   | [ "user" : "jsreport" ]
+        "ex_UnsupportedEncodingException"       | null                                  | null
+        "ex_URISyntaxException"                 | null                                  | null
+        "ex_JsReportException"                  | null                                  | null
+        "code_500"                              | null                                  | null
+        "code_404"                              | null                                  | null
+        "code_400"                              | null                                  | null
+    }
+
+    //
+    // Mocks
+    //
+
+    HttpRemoteService HTTP_REMOTE_SERVICE_MOCK = Mock(HttpRemoteService) {
+        post("/api/report", _) >> { def args ->
+            HttpResponse httpResponseMock = Mock(HttpResponse)
+
+            RenderTemplateRequest req = GSON.fromJson(
+                    new StringReader(args[1] as String),
+                    RenderTemplateRequest.class
+            )
+
+            def templateId = req.template.name
+
+            if (! templateId) {
+                templateId = req.template.shortid
+            }
+
+            if (templateId.startsWith("code_")) {
+                httpResponseMock.getStatusLine() >> { return new BasicStatusLine(new ProtocolVersion("HTTP", 1, 1), Integer.parseInt(templateId.substring(5)), "Test") }
+
+            } else if (templateId.startsWith("ex_")) {
+                String exType = templateId.substring(3)
+                switch (exType) {
+                    case "UnsupportedEncodingException": throw new UnsupportedEncodingException()
+                    case "URISyntaxException": throw new URISyntaxException("test", "test")
+                    case "JsReportException": throw new JsReportException()
+                    case "IOException": throw new IOException()
+                    default: throw new JsReportException()
+                }
+            }
+
+            HttpEntity httpEntityMock = Mock(HttpEntity)
+            httpResponseMock.getStatusLine() >> { return new BasicStatusLine(new ProtocolVersion("HTTP", 1, 1), 200, "Test") }
+            httpEntityMock.getContent() >> { return ReportingServiceImplSpec.getResourceAsStream("${templateId}.pdf") }
+            httpResponseMock.getEntity() >> { return httpEntityMock }
+
+            return httpResponseMock
+        }
+
+        findHeader(_, HttpRemoteServiceImpl.HEADER_CONTENT_TYPE) >> { return CONTENT_TYPE_HEADER }
+
+        findAndParseHeader(_, HttpRemoteServiceImpl.HEADER_FILE_EXTENSION) >> { return "" }
+    }
+
+    //
+    // Asserts
+    //
+
+    private static void assertReport(Report report, String requiredText) {
+        assert report != null
+        assert report.content != null
+        assert report.fileExtension == ""
+        assert report.contentType == CONTENT_TYPE_HEADER
+        assert report.getPermanentLink() == null
+
+        def load = PDDocument.load(report.content)
+        assert requiredText == PDF_TEXT_STRIPPER.getText(load)
+        load.close()
+    }
+
 }
